39e481ae1ec6f49f029c9c8bf6588a6efe6e1b95
Devgiri-code <gosai.devgiri.66@gmail.com>
2025-03-29 10:24:37 -0400

    commit

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "lab12-tfstate-rg"
-    storage_account_name = "lab12tfstated0f0006f" # Will be replaced after backend creation
+    storage_account_name = "lab12tfstated0f0006f" # Replace with your actual storage account name
     container_name      = "tfstate"
     key                 = "terraform.tfstate"
   }
